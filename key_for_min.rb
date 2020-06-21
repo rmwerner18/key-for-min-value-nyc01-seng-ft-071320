9da7c555436fe46f8f0ae0d0f8e56e36f3bfa571
Ryan Werner <rmwerner@umich.edu>
2020-06-21 17:32:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  answer = ""
+  min_value = name_hash[0].to_i
+  name_hash.each do |key, value|
+    if value.to_i < min_value
+      min_value = value.to_i
+      answer = key
+    end
+  end
 end
