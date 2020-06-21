118bb2cad01ab3c2436cea1e0ea7d916660a2727
Ryan Werner <rmwerner@umich.edu>
2020-06-21 17:36:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value.to_i < min_value
       min_value = value.to_i
-      answer = :key
+      answer = key
     end
   end
   answer 
